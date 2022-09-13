2933fbcaef4392806c91d883a89e3ece62e376b7
dumbkenstein <febriyannaufalahza@gmail.com>
2022-09-13 23:51:54 +0700

    Added changes

--- js/main.js
+++ js/main.js
@@ -10,4 +10,22 @@ $(document).ready(function(){
       $('.post-box').filter('.' + value).show('1000');
     }
   });
+  //Add active to btn
+  $('.filter-item').click(function() {
+    $(this).addClass("active-filter").siblings().removeClass("active-filter");
+  });
 });
+//Header background changes on scroll
+/*let header = document.querySelector("header");
+
+window.addEventListener('scroll', () => {
+  header.classList.toggle("show-dir", window.scrollY > 50);
+})*/
+function changeDir() {
+  document.getElementById("banner-dir").innerHTML=("$ cd article/");
+}
+
+var icon = document.getElementById("icon");
+icon.onclick = function(){
+  document.body.classList.toggle("light-mode");
+}
